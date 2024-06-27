966cdf87c520dec0cc54c343fffbe0d001b66c71
Dmitry Khlebnikov <dmitry.norge@gmail.com>
2024-06-28 00:58:34 +0200

    initialized project

---
+++ src/main/java/dmitrykh/local/springboot_randnumber/SpringbootRandnumberApplication.java
@@ -0,0 +1,13 @@
+package dmitrykh.local.springboot_randnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRandnumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRandnumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dmitrykh/local/springboot_randnumber/SpringbootRandnumberApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitrykh.local.springboot_randnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRandnumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
