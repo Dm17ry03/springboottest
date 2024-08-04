c67478665c29f4a61c7280621e03f21d1441d2dc
dmitrykh <dmitry.norge@gmail.com>
2024-08-04 20:42:24 +0200

    Changed the interface to use springboot

--- src/main/java/dmitrykh/local/springboot_randnumber/service/RandNumGenerator.java
+++ src/main/java/dmitrykh/local/springboot_randnumber/service/RandNumGenerator.java
@@ -1,7 +1,10 @@
 package dmitrykh.local.springboot_randnumber.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Random;
 
+@Service
 public class RandNumGenerator {
     Random rand = new Random();
 
