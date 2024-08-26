66ef46a04545f6afebad197fb413255cdced8a9e
dmitrykh <dmitry.norge@gmail.com>
2024-08-26 22:39:20 +0200

    Autowired randNumGenerator

--- src/test/java/dmitrykh/local/springboot_randnumber/service/RandNumGeneratorTest.java
+++ src/test/java/dmitrykh/local/springboot_randnumber/service/RandNumGeneratorTest.java
@@ -1,14 +1,23 @@
 package dmitrykh.local.springboot_randnumber.service;
 
 import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 class RandNumGeneratorTest {
+    RandNumGenerator randNumGenerator;
+
+    public RandNumGeneratorTest(@Autowired RandNumGenerator randNumGenerator) {
+        this.randNumGenerator = randNumGenerator;
+    }
+
 
     @RepeatedTest(10)
     void genNum() {
         Integer randlimit = 1000;
-        RandNumGenerator randNumGenerator = new RandNumGenerator();
         int result = randNumGenerator.genNum(randlimit);
         assertTrue(result < randlimit);
     }
